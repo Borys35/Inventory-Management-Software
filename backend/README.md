a8b792a4baa72dea6ad5a1e8bda13f05b434588a
Borys35 <boryskac10@gmail.com>
2025-11-24 19:59:38 +0100

    init postgresql

--- backend/README.md
+++ backend/README.md
@@ -1,1 +1,11 @@
 # Inventory-Management-Software
+
+Requirements:
+- Windows 11
+- PostgreSQL 18.1
+
+Local PostgreSQL:
+- name: postres
+- password: admin
+psql -U postgres
+create database inventory_db;
--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,17 @@
 from flask import Flask
+import psycopg2
+from lib.init_db import init_tables
 from blueprints.user_blueprint import user_blueprint
 
 app = Flask(__name__)
+
+conn = psycopg2.connect(database="inventory_db", user="postgres",
+                        password="admin", host="localhost", port="5432")
+
+cur = conn.cursor()
+
+init_tables(cur)
+
 app.register_blueprint(user_blueprint)
 
 @app.route("/")
