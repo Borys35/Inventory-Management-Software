09767bcbff37adaad40cd9bd95643a6cfcd0af0b
Borys35 <boryskac10@gmail.com>
2025-11-30 14:57:56 +0100

    complete supplier routes

--- backend/controllers/supplier_bp.py
+++ backend/controllers/supplier_bp.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, session, redirect, request, render_template, flash, g, jsonify
+from flask import Blueprint, session, redirect, request, render_template, flash, g, jsonify, url_for
 from lib.db import get_db_connection
 from lib.auth_middleware import login_required
 from models.supplier import Supplier
@@ -28,7 +28,7 @@ def create():
         flash("Incorrect data")
         return "Supplier create failed", 400
     
-@supplier_bp.route("/update/<supplier_id>", methods=['PUT'])
+@supplier_bp.route("/update/<supplier_id>", methods=['POST'])
 @login_required
 def update(supplier_id):
     data = request.form
@@ -47,12 +47,12 @@ def update(supplier_id):
 
     if supplier_id:
         flash(f"Supplier (ID={supplier_id}) updated successfully")
-        return jsonify(supplier_id)
+        return redirect(url_for('supplier.get_one', supplier_id=supplier_id))
     else:
         flash("Incorrect data")
         return "Supplier update failed", 400
     
-@supplier_bp.route("/delete/<supplier_id>", methods=['DELETE'])
+@supplier_bp.route("/delete/<supplier_id>", methods=['POST'])
 @login_required
 def delete(supplier_id):
     conn = get_db_connection()
@@ -66,7 +66,7 @@ def delete(supplier_id):
 
     if supplier_id:
         flash(f"Supplier (ID={supplier_id}) deleted successfully")
-        return jsonify(supplier_id)
+        return redirect(url_for('supplier.get_all'))
     else:
         flash("Incorrect data")
         return "Supplier delete failed", 400
@@ -85,7 +85,7 @@ def get_one(supplier_id):
     
     if supplier_id:
         flash(f"Supplier (ID={supplier_id}) get one successfully")
-        return jsonify(supplier)
+        return render_template('supplier.html', supplier=supplier)
     else:
         flash("Incorrect data")
         return "Supplier get one failed", 400
